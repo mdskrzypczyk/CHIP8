068ef29bd389d86c1f39578584a26bfed15be68f
mdskrzypczyk <mdskrzypczyk@tuta.io>
2021-07-11 20:28:33 -0500

    #22: Created unit tests for input code.

--- include/input.h
+++ include/input.h
@@ -4,8 +4,42 @@
 #include <SDL2/SDL.h>
 
 #include <iostream>
+#include <map>
 
 #define NUM_KEYS 16  // Chip 8 has 16 hexadecimal keys on its keyboard
+
+// Define keycodes
+#define KEY_0 0x0
+#define KEY_1 0x1
+#define KEY_2 0x2
+#define KEY_3 0x3
+#define KEY_4 0x4
+#define KEY_5 0x5
+#define KEY_6 0x6
+#define KEY_7 0x7
+#define KEY_8 0x8
+#define KEY_9 0x9
+#define KEY_A 0xA
+#define KEY_B 0xB
+#define KEY_C 0xC
+#define KEY_D 0xD
+#define KEY_E 0xE
+#define KEY_F 0xF
+#define KEY_QUIT 0x10
+#define KEY_COLOR_CHANGE 0x11
+#define KEY_SAVE 0x12
+#define KEY_LOAD 0x13
+#define KEY_ERR 0xFF
+
+// Default keymap
+static std::map<SDL_Keycode, uint8_t> keymap{
+        {SDLK_1, KEY_0},    {SDLK_2, KEY_1},      {SDLK_3, KEY_2},
+        {SDLK_4, KEY_3},    {SDLK_q, KEY_4},      {SDLK_w, KEY_5},
+        {SDLK_e, KEY_6},    {SDLK_r, KEY_7},      {SDLK_a, KEY_8},
+        {SDLK_s, KEY_9},    {SDLK_d, KEY_A},      {SDLK_f, KEY_B},
+        {SDLK_z, KEY_C},    {SDLK_x, KEY_D},      {SDLK_c, KEY_E},
+        {SDLK_v, KEY_F},    {SDL_QUIT, KEY_QUIT}, {SDLK_t, KEY_COLOR_CHANGE},
+        {SDLK_p, KEY_SAVE}, {SDLK_l, KEY_LOAD}};
 
 class INPUT {
   public:
@@ -18,6 +52,8 @@ class INPUT {
     // Function for updating key_status boolean array, checks the keyboard for
     // pressed/unpressed keys
     uint8_t poll_keyboard(SDL_Event event);
+    uint8_t handle_keydown(SDL_Keycode sym);
+    uint8_t handle_keyup(SDL_Keycode sym);
 
     // Function that returns pressed state of a given key
     bool get_key_status(uint8_t key);
--- src/input.cpp
+++ src/input.cpp
@@ -39,176 +39,12 @@ uint8_t INPUT::poll_keyboard(SDL_Event event) {
 
     // For handling key presses
     if (event.type == SDL_KEYDOWN && event.key.repeat == 0) {
-        switch (event.key.keysym.sym) {
-            case SDLK_1:
-                key_status[0] = true;
-                return_val = 0;
-                break;
-
-            case SDLK_2:
-                key_status[1] = true;
-                return_val = 1;
-                break;
-
-            case SDLK_3:
-                key_status[2] = true;
-                return_val = 2;
-                break;
-
-            case SDLK_4:
-                key_status[3] = true;
-                return_val = 3;
-                break;
-
-            case SDLK_q:
-                key_status[4] = true;
-                return_val = 4;
-                break;
-
-            case SDLK_w:
-                key_status[5] = true;
-                return_val = 5;
-                break;
-
-            case SDLK_e:
-                key_status[6] = true;
-                return_val = 6;
-                break;
-
-            case SDLK_r:
-                key_status[7] = true;
-                return_val = 7;
-                break;
-
-            case SDLK_a:
-                key_status[8] = true;
-                return_val = 8;
-                break;
-
-            case SDLK_s:
-                key_status[9] = true;
-                return_val = 9;
-                break;
-
-            case SDLK_d:
-                key_status[10] = true;
-                return_val = 10;
-                break;
-
-            case SDLK_f:
-                key_status[11] = true;
-                return_val = 11;
-                break;
-
-            case SDLK_z:
-                key_status[12] = true;
-                return_val = 12;
-                break;
-
-            case SDLK_x:
-                key_status[13] = true;
-                return_val = 13;
-                break;
-
-            case SDLK_c:
-                key_status[14] = true;
-                return_val = 14;
-                break;
-
-            case SDLK_v:
-                key_status[15] = true;
-                return_val = 15;
-                break;
-
-            // For color cycling
-            case SDLK_t:
-                return_val = 17;
-                break;
-
-            // For Save State loading/saving
-            case SDLK_p:
-                return_val = 18;
-                break;
-
-            case SDLK_l:
-                return_val = 19;
-                break;
-
-            default:
-                break;
-        }
+        return_val = handle_keydown(event.key.keysym.sym);
     }
 
     // Sets the key status to unpressed
     else if (event.type == SDL_KEYUP && event.key.repeat == 0) {
-        switch (event.key.keysym.sym) {
-            case SDLK_1:
-                key_status[0] = false;
-                break;
-
-            case SDLK_2:
-                key_status[1] = false;
-                break;
-
-            case SDLK_3:
-                key_status[2] = false;
-                break;
-
-            case SDLK_4:
-                key_status[3] = false;
-                break;
-
-            case SDLK_q:
-                key_status[4] = false;
-                break;
-
-            case SDLK_w:
-                key_status[5] = false;
-                break;
-
-            case SDLK_e:
-                key_status[6] = false;
-                break;
-
-            case SDLK_r:
-                key_status[7] = false;
-                break;
-
-            case SDLK_a:
-                key_status[8] = false;
-                break;
-
-            case SDLK_s:
-                key_status[9] = false;
-                break;
-
-            case SDLK_d:
-                key_status[10] = false;
-                break;
-
-            case SDLK_f:
-                key_status[11] = false;
-                break;
-
-            case SDLK_z:
-                key_status[12] = false;
-                break;
-
-            case SDLK_x:
-                key_status[13] = false;
-                break;
-
-            case SDLK_c:
-                key_status[14] = false;
-                break;
-
-            case SDLK_v:
-                key_status[15] = false;
-                break;
-
-            default:
-                break;
-        }
+        return_val = handle_keyup(event.key.keysym.sym);
     }
 
     // Handles clicking 'x' for window
@@ -217,6 +53,36 @@ uint8_t INPUT::poll_keyboard(SDL_Event event) {
     }
 
     return return_val;
+}
+
+uint8_t INPUT::handle_keydown(SDL_Keycode sym) {
+    if (keymap.count(sym) == 0) {
+        return KEY_ERR;
+    } else {
+        uint8_t key = keymap[sym];
+
+        // Only track key status for Chip-8 input
+        if (key <= KEY_F) {
+            key_status[key] = true;
+        }
+
+        return key;
+    }
+}
+
+uint8_t INPUT::handle_keyup(SDL_Keycode sym) {
+    if (keymap.count(sym) == 0) {
+        return KEY_ERR;
+    } else {
+        uint8_t key = keymap[sym];
+
+        // Only track key status for Chip-8 input
+        if (key <= KEY_F) {
+            key_status[key] = false;
+        }
+
+        return key;
+    }
 }
 
 /*
